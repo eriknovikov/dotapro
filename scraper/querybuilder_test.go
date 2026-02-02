6c21292f1a5a00e9ae62b8a9e74302ef0853f39e
Erik Novikov <eriknkv24@gmail.com>
2026-02-02 15:14:04 +0100

    Feat branch (#5)
    * commit on mock
    
    * commit on mock 2

--- scraper/querybuilder_test.go
+++ scraper/querybuilder_test.go
@@ -8,9 +8,11 @@ import (
 func TestQueryBuilder(t *testing.T) {
 	testMatchesIds := []int64{8673330227}
 	q := queryBuilder.GetMatches(testMatchesIds)
-	
+
 	if err := os.WriteFile("./od.query.sql", []byte(q), 0644); err != nil {
 		t.Fatalf("Failed to write query file: %v", err)
 	}
+	// dumb change
+	// change #2
 	t.Log("Query written to ./od.query.sql")
 }
